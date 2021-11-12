740a8049c55243bca3b2d5e7d272f9ad25b23a03
Anna Elvira Hartoyo <52845049+annaelvira24@users.noreply.github.com>
2021-11-12 23:19:04 +0700

    Update Lda2vec.py

--- lda2vec/Lda2vec.py
+++ lda2vec/Lda2vec.py
@@ -65,6 +65,8 @@ class Lda2vec:
 
 
         if not restore:
+            tf.reset_default_graph()
+
             self.date = datetime.now().strftime('%y%m%d_%H%M')
             self.logdir = ('{}_{}').format(self.logdir, self.date)
 
